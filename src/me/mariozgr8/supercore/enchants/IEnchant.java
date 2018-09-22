7dd10f727db575dc75eafbaf8451ab6ec2fc3553
Mariozgr8 <mbouzakhm20@ic.edu.lb>
2018-09-22 22:50:43 +0300

    Enchants Update #2

--- src/me/mariozgr8/supercore/enchants/IEnchant.java
+++ src/me/mariozgr8/supercore/enchants/IEnchant.java
@@ -1,12 +1,20 @@
 package me.mariozgr8.supercore.enchants;
 
-abstract class IEnchant {
+public abstract class IEnchant {
 	private int id;
 	private String name;
 	private int maxLevel;
 	
 	private EnchantType type;
 	private EnchantRarity rarity;
+	
+	public IEnchant(int id, String name, int maxLevel, EnchantType type, EnchantRarity rarity) {
+		this.id = id;
+		this.name = name;
+		this.maxLevel = maxLevel;
+		this.type = type;
+		this.rarity = rarity;
+	}
 	
 	public int getId() {
 		return id;
